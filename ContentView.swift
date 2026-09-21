9c700ef66cd126fe425fa835dd1b9eb6956f2f12
jorgeduba-maker <jorgeduba@gmail.com>
2026-09-21 11:05:33 +0200

    Create  ContentView.swift

---
+++ ContentView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("NutriTrainer")
+                .font(.largeTitle.bold())
+            Text("iPhone 17 Pro Max + Apple Watch")
+            Text("Control de comidas y macros")
+                .padding()
+        }
+    }
+}
